191780772c80e3989de51b56fe23e451f7b4115f
Christine Coulter <christinetcoulter@gmail.com>
2013-06-18 17:32:28 -0400

    Attempt to follow Ruby best practices

--- calculator.rb
+++ calculator.rb
@@ -7,92 +7,92 @@ PI = 3.14159265359
 #----------------------------GET NUMBERS-------------------------
 #Ask user for two numbers
 def two_numbers
-    puts "Enter the first number."
-    first_number = gets.chomp.to_f
-    puts "Enter the second number."
-    second_number = gets.chomp.to_f
-    return first_number, second_number
+  puts "Enter the first number."
+  first_number = gets.chomp.to_f
+  puts "Enter the second number."
+  second_number = gets.chomp.to_f
+  return first_number, second_number
 end
 
 #Ask user for radius
 def get_radius
-    puts "Enter the radius."
-    radius = gets.chomp.to_f
-    return radius
+  puts "Enter the radius."
+  radius = gets.chomp.to_f
+  return radius
 end
 
 #Ask user for a, b, and c for quadratic equation
 def quadratic_input
-    puts "Enter 'a'."
-    x = gets.chomp.to_f
-    puts "Enter 'b'."
-    y = gets.chomp.to_f
-    puts "Enter 'c'."
-    z = gets.chomp.to_f
-    return x, y, z
+  puts "Enter 'a'."
+  x = gets.chomp.to_f
+  puts "Enter 'b'."
+  y = gets.chomp.to_f
+  puts "Enter 'c'."
+  z = gets.chomp.to_f
+  return x, y, z
 end
 
 #Ask user for weight and height
 def bmi_values
-    puts "Enter your weight(lbs)."
-    m = gets.chomp.to_f
-    puts "Enter your height(inches)."
-    h = gets.chomp.to_f
-    return m, h
+  puts "Enter your weight(lbs)."
+  m = gets.chomp.to_f
+  puts "Enter your height(inches)."
+  h = gets.chomp.to_f
+  return m, h
 end
 
 #----------------------FUNCTIONS FOR CALCULATOR------------------
 #Add function
 def add(a, b)
-    puts a + b
+  puts a + b
 end
 
 #Subtract function
 def subtract(a, b)
-    puts a - b
+  puts a - b
 end
 
 #Multiply function
 def multiply(a, b)
-    puts a * b
+  puts a * b
 end
 
 #Divide function
 def divide(a, b)
-    puts a / b
+  puts a / b
 end
 
 #Area of a circle function
 def area_of_circle(r)
-    puts PI * (r**2)
+  puts PI * (r**2)
 end
 
 #Circumference of a circle function
 def circumference_of_circle(r)
-    puts PI * 2 * r
+  puts PI * 2 * r
 end
 
 #Quadratic equation function
 #(-b +/- sqroot((b^2) - 4ac)) / 2a
 def quadratic_equation(a, b, c)
-    first_part = -b / (2 * a)
-    second_part = (((b**2) - (4 * a * c))**(0.5)) / (2 * a)
-    first_answer = first_part + second_part
-    second_answer = first_part - second_part
-    puts "#{first_answer} or #{second_answer}."
+  first_part = -b / (2 * a)
+  second_part = (((b**2) - (4 * a * c))**(0.5)) / (2 * a)
+  first_answer = first_part + second_part
+  second_answer = first_part - second_part
+  puts "#{first_answer} or #{second_answer}."
 end
 
 #BMI function
 def bmi_equation(mass, height)
-    bmi = (mass / (height**2)) * 703
-    if bmi < 18.5
-        nih = "underweight"
-    elsif bmi > 25
-        nih = "overweight"
-    else
-        nih = "normal"
-    end
-    puts "Your BMI is #{bmi}. According to the National Institutes of Health, you are considered #{nih}."
+  bmi = (mass / (height**2)) * 703
+  if bmi < 18.5
+    nih = "underweight"
+  elsif bmi > 25
+    nih = "overweight"
+  else
+    nih = "normal"
+  end
+  puts "Your BMI is #{bmi}. According to the National Institutes of Health, you are considered #{nih}."
 end
 
 
